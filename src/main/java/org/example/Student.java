d646aef07a96869f525d1873586167a0798ad577
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-06-12 18:50:20 +0530

    Initial Commit

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Student
+{
+    String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
